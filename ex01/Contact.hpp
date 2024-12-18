e277ef2efcdb62567c0949f907ea0d64e7274a85
Irene Puig Parnau <ipuig-pa@1-D-1.42heilbronn.de>
2024-12-18 17:29:12 +0100

    preparation for ex01

---
+++ ex01/Contact.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ipuig-pa <ipuig-pa@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 16:58:04 by ipuig-pa          #+#    #+#             */
+/*   Updated: 2024/12/18 17:02:02 by ipuig-pa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <iostrem>
+# include <>
+# include 
+
+class Contract
+{
+	private:
+
+	public:
+
+};
+
+#endif
